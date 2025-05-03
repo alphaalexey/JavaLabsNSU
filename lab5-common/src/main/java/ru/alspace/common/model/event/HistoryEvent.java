fb7719c5af50aefb57bb0810563fbe001535d50a
alphaalexey <homyakovost@gmail.com>
2025-05-03 17:01:43 +0700

    Remove unused HistoryEvent.java

--- lab5-common/src/main/java/ru/alspace/common/model/event/HistoryEvent.java
+++
@@ -1,8 +0,0 @@
-package ru.alspace.common.model.event;
-
-import ru.alspace.common.model.data.ChatMessage;
-
-import java.util.List;
-
-public record HistoryEvent(List<ChatMessage> history) implements Event {
-}
